865def7b362d2cd90395a948faf82845205e8987
Emil Nikolov <emil.e.nikolov@gmail.com>
2018-02-20 15:28:07 +0100

    added WithFormat

--- xexec/xexec.go
+++ xexec/xexec.go
@@ -6,44 +6,53 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	
+
 	"strings"
-	
+
 	"bufio"
 	"bytes"
-	
+
 	"github.com/pkg/errors"
 )
 
 type Options struct {
-	dir  string
-	args []string
+	dir     string
+	args    []string
+	fmtArgs []interface{}
 }
 
 type OptionFunc func(*Options)
 
-
 // Run is an experimental run, does not work if it contains spaces inside command arguments or paths
 func Run(command string, optFuncs ...OptionFunc) ([]byte, error) {
 	options := &Options{}
-	
 	for _, optFunc := range optFuncs {
 		optFunc(options)
 	}
-	
+
+	if len(options.fmtArgs) != 0 {
+		command = fmt.Sprintf(command, options.fmtArgs...)
+	}
+
 	if len(options.args) != 0 {
 		command += " " + strings.Join(options.args, " ")
 	}
-	
+
 	cmd := exec.Command("sh", "-c", command)
-	
+
 	if options.dir != "" {
 		cmd.Dir = options.dir
 	}
-	
+
 	log.Printf("Executing %s\n", command)
-	
+
 	return run(cmd)
+}
+
+func WithFormat(fmtArgs ...interface{}) OptionFunc {
+	return func(o *Options) {
+		o.fmtArgs = fmtArgs
+	}
 }
 
 func WithDir(dir string) OptionFunc {
@@ -58,53 +67,52 @@ func WithArgs(args ...string) OptionFunc {
 	}
 }
 
-
 func run(cmd *exec.Cmd) ([]byte, error) {
 	// To fix a path issue caused by automatic path conversions in some windows shells, see https://github.com/docker/toolbox/issues/282
 	cmd.Env = getEnv()
-	
+
 	stdout, err := cmd.StdoutPipe()
-	
+
 	if err != nil {
 		return nil, errors.Wrap(err, "")
 	}
-	
+
 	stderr, err := cmd.StderrPipe()
-	
+
 	if err != nil {
 		return nil, errors.Wrap(err, "")
 	}
-	
+
 	err = cmd.Start()
-	
+
 	if err != nil {
 		return nil, errors.Wrap(err, "")
 	}
-	
+
 	var b bytes.Buffer
-	
+
 	out := bufio.NewWriter(&b)
-	
+
 	go copyPipe(stdout, out, os.Stdout)
 	go copyPipe(stderr, os.Stderr)
-	
+
 	err = cmd.Wait()
-	
+
 	if err != nil {
 		return nil, errors.Wrap(err, "")
 	}
-	
+
 	err = out.Flush()
-	
+
 	if err != nil {
 		return nil, errors.Wrap(err, "")
 	}
-	
+
 	if !cmd.ProcessState.Success() {
 		//log.Printf("Failed to exec command\n")
 		return nil, errors.New("failed to exec command")
 	}
-	
+
 	//log.Printf("Success\n")
 	return b.Bytes(), nil
 }
@@ -124,9 +132,9 @@ func getEnv() []string {
 
 func copyPipe(src io.ReadCloser, dsts ...io.Writer) {
 	var err error
-	
+
 	buff := make([]byte, 100)
-	
+
 	var n int
 	for err == nil {
 		n, err = src.Read(buff)
