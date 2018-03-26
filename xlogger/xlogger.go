69926f09185ee5ffae4d2e9aa64ca3c4a37ac94a
Emil Nikolov <emil.e.nikolov@gmail.com>
2018-03-26 10:08:36 +0200

    force colors with text format

--- xlogger/xlogger.go
+++ xlogger/xlogger.go
@@ -40,6 +40,7 @@ func New(cfg *Config) (*Logger, error) {
 		formatter = &logrus.TextFormatter{
 			TimestampFormat: "2006-01-02 15:04:05",
 			FullTimestamp:   true,
+			ForceColors:     true,
 		}
 	case "json":
 		fallthrough
