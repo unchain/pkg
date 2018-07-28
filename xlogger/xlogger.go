de579a09dc8b518b7155f60c1ef50a245e480350
enigma <ng2dev@gmail.com>
2018-07-28 18:25:29 +0200

    add hide fname option

--- xlogger/xlogger.go
+++ xlogger/xlogger.go
@@ -8,28 +8,39 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	"github.com/pkg/errors"
+	"github.com/unchainio/pkg/xmerge"
 )
 
 type Config struct {
-	Level  string
-	Format string
+	Level     string
+	Format    string
+	HideFName bool
 }
 
 type Logger struct {
-	log *logrus.Logger
-	cfg *Config
+	log    *logrus.Logger
+	errLog *logrus.Logger
+	cfg    *Config
 }
 
-func New(cfg *Config) (*Logger, error) {
+var defaultConfig = &Config{
+	Level:  "debug",
+	Format: "text",
+}
 
-	if cfg.Level == "" {
-		cfg.Level = "debug"
+func New(input *Config) (*Logger, error) {
+	cfg := defaultConfig
+
+	err := xmerge.Merge(cfg, input)
+
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to merge with default config.")
 	}
 
 	logLevel, err := logrus.ParseLevel(cfg.Level)
 
 	if err != nil {
-		return nil, errors.Wrap(err, "Unsupported log level")
+		return nil, errors.Wrap(err, "unsupported log level")
 	}
 
 	var formatter logrus.Formatter
@@ -37,9 +48,9 @@ func New(cfg *Config) (*Logger, error) {
 	switch cfg.Format {
 	case "text":
 		formatter = &logrus.TextFormatter{
-		//DisableColors: true,
-		//DisableSorting:   true,
-		//QuoteEmptyFields: false,
+			TimestampFormat: "2006-01-02 15:04:05",
+			FullTimestamp:   true,
+			ForceColors:     true,
 		}
 	case "json":
 		fallthrough
@@ -50,112 +61,121 @@ func New(cfg *Config) (*Logger, error) {
 	}
 
 	l := &Logger{
-		cfg: cfg,
 		log: &logrus.Logger{
-			Out:       os.Stderr,
-			Formatter: formatter,
+			Out:       os.Stdout,
 			Hooks:     make(logrus.LevelHooks),
+			Formatter: formatter,
 			Level:     logLevel,
 		},
+		errLog: &logrus.Logger{
+			Out:       os.Stderr,
+			Hooks:     make(logrus.LevelHooks),
+			Formatter: formatter,
+			Level:     logLevel,
+		},
+		cfg: cfg,
 	}
 
 	return l, nil
 }
 
-func (l *Logger) Log() *logrus.Entry {
+func (l *Logger) Log(log *logrus.Logger) *logrus.Entry {
 	if pc, file, line, ok := runtime.Caller(2); ok {
 		fName := runtime.FuncForPC(pc).Name()
 
 		currentDir, _ := os.Getwd()
-		file = strings.Replace(file, currentDir, "", -1)
+		file = strings.Replace(file, currentDir+"/", "", -1)
 
 		caller := fmt.Sprintf("%s:%v", file, line)
 
-		return l.log.WithField("caller", caller).WithField("fName", fName)
+		if l.cfg.HideFName {
+			return log.WithField("caller", caller)
+		}
+		return log.WithField("caller", caller).WithField("fName", fName)
 	}
 	return &logrus.Entry{}
 }
 
 func (l *Logger) Printf(format string, v ...interface{}) {
-	l.Log().Printf(format, v...)
+	l.Log(l.log).Printf(format, v...)
 }
 
 func (l *Logger) Fatalf(format string, v ...interface{}) {
-	l.Log().Fatalf(format, v...)
+	l.Log(l.errLog).Fatalf(format, v...)
 }
 
 func (l *Logger) Panicf(format string, v ...interface{}) {
-	l.Log().Panicf(format, v...)
+	l.Log(l.errLog).Panicf(format, v...)
 }
 
 func (l *Logger) Debugf(format string, v ...interface{}) {
-	l.Log().Debugf(format, v...)
+	l.Log(l.log).Debugf(format, v...)
 }
 
 func (l *Logger) Warnf(format string, v ...interface{}) {
-	l.Log().Warnf(format, v...)
+	l.Log(l.log).Warnf(format, v...)
 }
 
 func (l *Logger) Warningf(format string, v ...interface{}) {
-	l.Log().Warningf(format, v...)
+	l.Log(l.log).Warningf(format, v...)
 }
 
 func (l *Logger) Errorf(format string, v ...interface{}) {
-	l.Log().Errorf(format, v...)
+	l.Log(l.errLog).Errorf(format, v...)
 }
 
 func (l *Logger) Print(v ...interface{}) {
-	l.Log().Print(v...)
+	l.Log(l.log).Print(v...)
 }
 
 func (l *Logger) Fatal(v ...interface{}) {
-	l.Log().Fatal(v...)
+	l.Log(l.errLog).Fatal(v...)
 }
 
 func (l *Logger) Panic(v ...interface{}) {
-	l.Log().Panic(v...)
+	l.Log(l.errLog).Panic(v...)
 }
 
 func (l *Logger) Debug(v ...interface{}) {
-	l.Log().Debug(v...)
+	l.Log(l.log).Debug(v...)
 }
 
 func (l *Logger) Warn(v ...interface{}) {
-	l.Log().Warn(v...)
+	l.Log(l.log).Warn(v...)
 }
 
 func (l *Logger) Warning(v ...interface{}) {
-	l.Log().Warning(v...)
+	l.Log(l.log).Warning(v...)
 }
 
 func (l *Logger) Error(v ...interface{}) {
-	l.Log().Error(v...)
+	l.Log(l.errLog).Error(v...)
 }
 
 func (l *Logger) Println(v ...interface{}) {
-	l.Log().Println(v...)
+	l.Log(l.log).Println(v...)
 }
 
 func (l *Logger) Fatalln(v ...interface{}) {
-	l.Log().Fatalln(v...)
+	l.Log(l.errLog).Fatalln(v...)
 }
 
 func (l *Logger) Panicln(v ...interface{}) {
-	l.Log().Panicln(v...)
+	l.Log(l.errLog).Panicln(v...)
 }
 
 func (l *Logger) Debugln(v ...interface{}) {
-	l.Log().Debugln(v...)
+	l.Log(l.log).Debugln(v...)
 }
 
 func (l *Logger) Warnln(v ...interface{}) {
-	l.Log().Warnln(v...)
+	l.Log(l.log).Warnln(v...)
 }
 
 func (l *Logger) Warningln(v ...interface{}) {
-	l.Log().Warningln(v...)
+	l.Log(l.log).Warningln(v...)
 }
 
 func (l *Logger) Errorln(v ...interface{}) {
-	l.Log().Errorln(v...)
+	l.Log(l.errLog).Errorln(v...)
 }
