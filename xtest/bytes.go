dae086b6ea4dc1fce01fb545b5abee5baabd0f7f
Emil Nikolov <emil.e.nikolov@gmail.com>
2019-02-25 23:57:16 +0100

    xtest: added WriteBytes

--- xtest/bytes.go
+++ xtest/bytes.go
@@ -4,15 +4,20 @@ import (
 	"io/ioutil"
 	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 // LoadBytes is a test helper that returns the bytes of a file with path relative to testdata/
 func LoadBytes(t *testing.T, path string) []byte {
 	bytes, err := ioutil.ReadFile(filepath.Join("testdata", path))
-
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	return bytes
+}
+
+// LoadBytes is a test helper that returns the bytes of a file with path relative to testdata/
+func WriteBytes(t *testing.T, path string, bytes []byte) {
+	err := ioutil.WriteFile(filepath.Join("testdata", path), bytes, 0644)
+	require.NoError(t, err)
 }
